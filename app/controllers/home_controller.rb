bfa032d54a2c5e6c14295313229ea70e7d0c65a9
max.jos <maxim.j@tymate.com>
2023-04-18 17:25:38 +0200

    feat: handle error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  rescue_from RuntimeError, with: :handle_runtime_error
+
   def index
     @response = ''
   end
@@ -8,5 +10,14 @@ class HomeController < ApplicationController
   def ask
     @response = ChatgptService.call(params[:question])
     redirect_to root_path(response: @response)
+    flash[:error] = "Une erreur s'est produite lors de la réponse à votre question. Veuillez réessayer plus tard."
+    redirect_to root_path
+  end
+
+  private
+
+  def handle_runtime_error(exception)
+    flash[:error] = "Une erreur est survenue : #{exception.message}"
+    redirect_to root_path
   end
 end
